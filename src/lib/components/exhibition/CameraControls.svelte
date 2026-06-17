663949f287922b67f0687a273bda4917b1b424b8
guicoelhodev <gs.coelho_dev@outlook.com>
2026-06-17 20:39:24 -0300

    feat(CameraControls.svelte): add room orbit camera

---
+++ src/lib/components/exhibition/CameraControls.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { T } from '@threlte/core';
+	import { OrbitControls } from '@threlte/extras';
+</script>
+
+<T.PerspectiveCamera makeDefault position={[0, 2.2, 5.8]} fov={54}>
+	<OrbitControls
+		target={[0, 1.35, -1.2]}
+		enableDamping
+		enablePan={false}
+		maxPolarAngle={Math.PI / 2.05}
+		minDistance={2.6}
+		maxDistance={7.5}
+	/>
+</T.PerspectiveCamera>
