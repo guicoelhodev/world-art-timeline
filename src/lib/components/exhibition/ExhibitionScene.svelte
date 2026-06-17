7a3b05ec6e1aece9ef83b6a06f7ff66bee9031ce
guicoelhodev <gs.coelho_dev@outlook.com>
2026-06-17 20:39:24 -0300

    feat(ExhibitionScene.svelte): mount threlte exhibition canvas

---
+++ src/lib/components/exhibition/ExhibitionScene.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Canvas } from '@threlte/core';
+	import SceneStage from './SceneStage.svelte';
+	import type { Artist, Artwork } from '$lib/types/domain';
+
+	let {
+		artist,
+		artworks,
+		onSelect
+	}: { artist: Artist; artworks: Artwork[]; onSelect: (artwork: Artwork) => void } = $props();
+</script>
+
+<div class="h-full min-h-[620px] w-full touch-none">
+	<Canvas>
+		<SceneStage {artist} {artworks} {onSelect} />
+	</Canvas>
+</div>
