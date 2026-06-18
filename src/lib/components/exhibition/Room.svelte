b36128e9814c8e0ae5ee75106295f686b3cf74ce
guicoelhodev <gs.coelho_dev@outlook.com>
2026-06-17 23:09:33 -0300

    refactor(Room.svelte): adjust room geometry

--- src/lib/components/exhibition/Room.svelte
+++ src/lib/components/exhibition/Room.svelte
@@ -7,27 +7,27 @@
 	<T.MeshStandardMaterial color="#9e9990" roughness={0.9} />
 </T.Mesh>
 
-<T.Mesh position={[0, 5.5, 0]} rotation.x={Math.PI / 2}>
+<T.Mesh position={[0, 6, 0]} rotation.x={Math.PI / 2}>
 	<T.PlaneGeometry args={[14, 30]} />
-	<T.MeshStandardMaterial color="#d6d1c7" roughness={1} />
+	<T.MeshStandardMaterial color="#d6d1c6" roughness={1} />
 </T.Mesh>
 
-<T.Mesh position={[0, 2.75, -15]}>
-	<T.PlaneGeometry args={[14, 5.5]} />
-	<T.MeshStandardMaterial color="#d6d1c7" roughness={0.85} />
+<T.Mesh position={[0, 3, -15]}>
+	<T.PlaneGeometry args={[14, 6]} />
+	<T.MeshStandardMaterial color="#d6d1c6" roughness={0.85} />
 </T.Mesh>
 
-<T.Mesh position={[-7, 2.75, 0]} rotation.y={Math.PI / 2}>
-	<T.PlaneGeometry args={[30, 5.5]} />
-	<T.MeshStandardMaterial color="#d6d1c7" roughness={0.9} />
+<T.Mesh position={[-6, 3, 0]} rotation.y={Math.PI / 2}>
+	<T.PlaneGeometry args={[30, 6]} />
+	<T.MeshStandardMaterial color="#d6d1c6" roughness={0.9} />
 </T.Mesh>
 
-<T.Mesh position={[7, 2.75, 0]} rotation.y={-Math.PI / 2}>
-	<T.PlaneGeometry args={[30, 5.5]} />
-	<T.MeshStandardMaterial color="#d6d1c7" roughness={0.9} />
+<T.Mesh position={[6, 3, 0]} rotation.y={-Math.PI / 2}>
+	<T.PlaneGeometry args={[30, 6]} />
+	<T.MeshStandardMaterial color="#d6d1c6" roughness={0.9} />
 </T.Mesh>
 
-<T.Mesh position={[0, 2.75, 15]} rotation.y={Math.PI}>
-	<T.PlaneGeometry args={[14, 5.5]} />
-	<T.MeshStandardMaterial color="#d6d1c7" roughness={0.85} />
+<T.Mesh position={[0, 3, 15]} rotation.y={Math.PI}>
+	<T.PlaneGeometry args={[14, 6]} />
+	<T.MeshStandardMaterial color="#d6d1c6" roughness={0.85} />
 </T.Mesh>
