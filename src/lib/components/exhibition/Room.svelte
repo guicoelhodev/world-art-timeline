15be84973e4c14f0279706ac1718f4e8734017cd
guicoelhodev <gs.coelho_dev@outlook.com>
2026-06-17 21:10:31 -0300

    feat(Room.svelte): enlarge gallery dimensions

--- src/lib/components/exhibition/Room.svelte
+++ src/lib/components/exhibition/Room.svelte
@@ -3,26 +3,26 @@
 </script>
 
 <T.Mesh position={[0, 0, 0]} rotation.x={-Math.PI / 2} receiveShadow>
-	<T.PlaneGeometry args={[8, 8]} />
+	<T.PlaneGeometry args={[14, 14]} />
 	<T.MeshStandardMaterial color="#d8d0c3" roughness={0.9} />
 </T.Mesh>
 
-<T.Mesh position={[0, 3, 0]} rotation.x={Math.PI / 2}>
-	<T.PlaneGeometry args={[8, 8]} />
+<T.Mesh position={[0, 5.5, 0]} rotation.x={Math.PI / 2}>
+	<T.PlaneGeometry args={[14, 14]} />
 	<T.MeshStandardMaterial color="#eee9df" roughness={1} />
 </T.Mesh>
 
-<T.Mesh position={[0, 1.5, -4]}>
-	<T.PlaneGeometry args={[8, 3]} />
+<T.Mesh position={[0, 2.75, -7]}>
+	<T.PlaneGeometry args={[14, 5.5]} />
 	<T.MeshStandardMaterial color="#f1eadf" roughness={0.85} />
 </T.Mesh>
 
-<T.Mesh position={[-4, 1.5, 0]} rotation.y={Math.PI / 2}>
-	<T.PlaneGeometry args={[8, 3]} />
+<T.Mesh position={[-7, 2.75, 0]} rotation.y={Math.PI / 2}>
+	<T.PlaneGeometry args={[14, 5.5]} />
 	<T.MeshStandardMaterial color="#e6ded2" roughness={0.9} />
 </T.Mesh>
 
-<T.Mesh position={[4, 1.5, 0]} rotation.y={-Math.PI / 2}>
-	<T.PlaneGeometry args={[8, 3]} />
+<T.Mesh position={[7, 2.75, 0]} rotation.y={-Math.PI / 2}>
+	<T.PlaneGeometry args={[14, 5.5]} />
 	<T.MeshStandardMaterial color="#e6ded2" roughness={0.9} />
 </T.Mesh>
