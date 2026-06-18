ff096084962c9a46e6f026d3310f4f448c58d5f4
guicoelhodev <gs.coelho_dev@outlook.com>
2026-06-17 22:25:14 -0300

    style(Room.svelte): darken wall colors

--- src/lib/components/exhibition/Room.svelte
+++ src/lib/components/exhibition/Room.svelte
@@ -4,30 +4,30 @@
 
 <T.Mesh position={[0, 0, 0]} rotation.x={-Math.PI / 2} receiveShadow>
 	<T.PlaneGeometry args={[14, 30]} />
-	<T.MeshStandardMaterial color="#d8d0c3" roughness={0.9} />
+	<T.MeshStandardMaterial color="#9e9990" roughness={0.9} />
 </T.Mesh>
 
 <T.Mesh position={[0, 5.5, 0]} rotation.x={Math.PI / 2}>
 	<T.PlaneGeometry args={[14, 30]} />
-	<T.MeshStandardMaterial color="#eee9df" roughness={1} />
+	<T.MeshStandardMaterial color="#d6d1c7" roughness={1} />
 </T.Mesh>
 
 <T.Mesh position={[0, 2.75, -15]}>
 	<T.PlaneGeometry args={[14, 5.5]} />
-	<T.MeshStandardMaterial color="#f1eadf" roughness={0.85} />
+	<T.MeshStandardMaterial color="#d6d1c7" roughness={0.85} />
 </T.Mesh>
 
 <T.Mesh position={[-7, 2.75, 0]} rotation.y={Math.PI / 2}>
 	<T.PlaneGeometry args={[30, 5.5]} />
-	<T.MeshStandardMaterial color="#e6ded2" roughness={0.9} />
+	<T.MeshStandardMaterial color="#d6d1c7" roughness={0.9} />
 </T.Mesh>
 
 <T.Mesh position={[7, 2.75, 0]} rotation.y={-Math.PI / 2}>
 	<T.PlaneGeometry args={[30, 5.5]} />
-	<T.MeshStandardMaterial color="#e6ded2" roughness={0.9} />
+	<T.MeshStandardMaterial color="#d6d1c7" roughness={0.9} />
 </T.Mesh>
 
 <T.Mesh position={[0, 2.75, 15]} rotation.y={Math.PI}>
 	<T.PlaneGeometry args={[14, 5.5]} />
-	<T.MeshStandardMaterial color="#f1eadf" roughness={0.85} />
+	<T.MeshStandardMaterial color="#d6d1c7" roughness={0.85} />
 </T.Mesh>
